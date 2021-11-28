d06ba01eea1848141fc252dac4eb736162cd8064
insung <leeinsung12@gmail.com>
2021-11-28 16:24:22 +0900

    database_insung

--- server/src/database.c
+++ server/src/database.c
@@ -9,10 +9,13 @@ function : read database or write database
 #include <fcntl.h>
 
 #define database "./data/database.csv"
+#define DB_U "./data/DB_user"
+#define DB_C "./data/DB_Chat"
 #define TRUE 1
 #define FALSE 0
 #define DEBUG 1 //when debugging, set value 1
-
+FILE* fp_User; //유저 관련 파일포인터
+FILE* fp_Chat; //채팅 관련 파일포인터
 
 
 //extern function test
@@ -30,6 +33,25 @@ void isPossibleName(char *userName)
     sendData(userName, "", 0);
 
     close(fd);
+}
+
+void initDB(){
+
+    fp_User=fopen(DB_U,"r+");
+    fp_Chat=fopen(DB_C,"r+");
+
+    fprintf(fp_User,"USER_NAME,USER_IP,IS_BAN\n");
+    fprintf(fp_Chat,"ID,FROM,TO,CONTENT,TIME\n");
+
+}
+
+//구조체를 쓰면 어떨까?
+void WriteName(long int user_ip, char* username,int IsBan) {
+    fprintf(fp_User, "%s,%ld,%d\n", username, user_ip, IsBan);
+}
+
+void WriteMessage(long int m_id,long int From_id,long int To_id,char* content,int time) {
+    fprintf(fp_Chat, "%ld,%ld,%ld,%s,%d\n", m_id, From_id, To_id, content, time);
 }
 
 // No main function in this file.
